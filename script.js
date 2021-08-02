7c2add1a19f786af3c4bd7ea548d37521b50ff17
Lauren Hess <laurenbethhess@gmail.com>
2021-08-02 16:10:30 -0400

    updated content to make it a tutorial

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "Welcome!  This is an example on how to add content from Javascript!"; 
+h2.textContent = "Welcome!  This is an example on how to add content from Javascript into the document!"; 
 document.querySelector("body").appendChild(h2); 
 
