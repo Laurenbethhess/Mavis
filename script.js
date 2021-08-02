dbc4706b81611d1dc600557f3cce2327fd8e25ad
Lauren Hess <laurenbethhess@gmail.com>
2021-08-02 15:40:21 -0400

    added content with js

---
+++ script.js
@@ -0,0 +1,4 @@
+const h2 = document.createElement("h2");
+h2.textContent = "Welcome!  This is an example on how to add content from Javascript!"; 
+document.querySelector("body").appendChild(h2); 
+
