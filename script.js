a2a0420b585862acedc45425e43c2d2bc0eae0c2
Lauren Hess <laurenbethhess@gmail.com>
2021-08-02 20:13:38 -0400

    displayed divs side by side

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "Welcome!  This is an example on how to add content from Javascript into the document!"; 
+h2.textContent = "Welcome to my first webpage to showcase some of the tricks I learned in my pre-work at Flatiron School!  This is an example of how to add content from a linked javascript file."; 
 document.querySelector("body").appendChild(h2); 
-
