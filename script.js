4a3a93f7569e2b67bc41d2bc549f985f04a5eae6
Lauren Hess <laurenbethhess@gmail.com>
2021-08-03 09:56:25 -0400

    changed the styling more

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "Welcome to my first webpage to showcase some of the tricks I learned in my pre-work at Flatiron School!  This is an example of how to add content from a linked javascript file."; 
+h2.textContent = "Welcome to my first webpage to showcase some of the skills I learned in my pre-work at Flatiron School!  This is an example of how to add content from a linked javascript file."; 
 document.querySelector("body").appendChild(h2); 
